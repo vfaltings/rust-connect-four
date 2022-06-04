574e36b648aa6a99183017d23d4159629de88237
Victor Faltings <victor.faltings@epfl.ch>
2022-06-04 22:28:07 +0200

    added display function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-fn main() {
-    setup();
+const BOARD_SIZE: usize = 10;
 
-    loop {
-        play_one_round();
-        check_if_over();
-        display_board();
+fn main() {
+    let mut gameboard = [[0; BOARD_SIZE]; BOARD_SIZE];
+    display_board(&gameboard);
+}
+
+fn display_board(board: &[[i32; BOARD_SIZE]; BOARD_SIZE]) {
+    for row in board {
+        for elem in row {
+            print!("{} ", elem);
+        }
+        println!();
     }
 }
